4e601750a0f60fbaffb788997cb10008a36d003b
via reed <viareed.cis@gmai.com>
2024-07-30 15:27:54 +0530

    class component added

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -3,11 +3,13 @@ import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import About from "./components/About";
+import ClassComponent from "./ClassComponent";
 
 const App = () => {
   return (
     <Router>
       <Navbar />
+      <ClassComponent />
       <Routes>
         <Route path="/" element={<TextForm />} />
         <Route path="About" element={<About />} />
---
+++ first-react-app/src/ClassComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class ClassComponent extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Class - Component</h2>
+      </div>
+    );
+  }
+}
