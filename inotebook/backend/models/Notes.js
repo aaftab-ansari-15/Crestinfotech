d95dc7d200ca41a4860b5d2d3cbd8b017a314fcc
via reed <viareed.cis@gmai.com>
2024-08-01 19:00:14 +0530

    today last change

--- inotebook/backend/models/Notes.js
+++ inotebook/backend/models/Notes.js
@@ -1,8 +1,7 @@
-const { type } = require("@testing-library/user-event/dist/type");
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 const NoteSchema = new Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'user'},
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
   title: { type: String, required: true },
   description: { type: String, required: true },
   tag: { type: String, default: "genral" },
