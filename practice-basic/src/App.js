517a46b6b9fe29e4996924e58651db419a001e53
Aftab K. Ansari <68555868+aaftab-ansari-15@users.noreply.github.com>
2024-09-02 11:16:29 +0530

    pr1 commited

---
+++ practice-basic/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import { Pr1 } from "./component/Pr1";
+
+function App() {
+  return (
+    <div className="App">
+      <Pr1 />
+    </div>
+  );
+}
+
+export default App;
---
+++ practice-basic/src/component/Pr1.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+export const Pr1 = () => {
+  const [count, setCount] = useState(0);
+  console.log("UseState render");
+  return (
+    <div>
+      <button
+        onClick={(c) => {
+          setCount(c + 1);
+        }}
+      >
+        count - {count}
+      </button>
+      <button
+        onClick={() => {
+          setCount(0);
+        }}
+      >
+        count to 0
+      </button>
+      <button
+        onClick={() => {
+          setCount(5);
+        }}
+      >
+        count to 5
+      </button>
+    </div>
+  );
+};
